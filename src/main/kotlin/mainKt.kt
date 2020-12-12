6207c2e79d5d913a87f26bbd469fccf4b8156f08
unknown <alexander.peshkov27@gmail.com>
2020-12-12 20:40:35 +0000

    Разная коммисия

--- src/main/kotlin/mainKt.kt
+++ src/main/kotlin/mainKt.kt
@@ -1,22 +1,48 @@
+import java.lang.IllegalArgumentException
 import java.util.*
 
-fun main(): Unit {
-    println("Enter amount you would like to transfer:")
+fun main() {
     val scanner = Scanner(System.`in`).useLocale(Locale.US)
-    val amount : Double = scanner.nextDouble() // in rubles
-    println("You entered : $amount rub")
-    val finalFee = calculateFee(amount)
-    println("Fee for money transfer : $finalFee rub")
+    println("Введите тип карты (VkPlay, MasterCard, Maestro, Visa, Мир):")
+    val cardIssuer = scanner.next()
+    println("Введите cумму предыдущих переводов в этом месяце:")
+    val lastTransferSum = scanner.nextInt()
+    println("Введите cумму для перевода:")
+    val currentTransferSum = scanner.nextInt()
+    println("Ваша комиссия = ${payFee(cardIssuer, lastTransferSum, currentTransferSum)} копейек")
+
 }
 
-
-fun calculateFee(amount : Double) : Double {
-    val percent2Decimal = 0.75 / 100
-    val fee = amount * percent2Decimal
-    println("Fee : $fee")
-    return if (fee <= 35) {
-        35.00
-    } else {
-        fee
+fun payFee(cardIssuer: String = "VkPlay", lastTransferSum: Int, currentTransferSum: Int):  Double {
+    println("Расчет для $cardIssuer")
+    val userTransfersInMonth = lastTransferSum + currentTransferSum
+    val fee = when {
+        cardIssuer.equals("VkPlay") -> {
+            if (currentTransferSum >= 15_000 && (userTransfersInMonth <= 40_0000))
+                throw IllegalArgumentException("Вы превысили лимит! Ваш перевод = $currentTransferSum, ваши переводы в этом месяце = $userTransfersInMonth")
+            else 0
+        }
+        cardIssuer in listOf("MasterCard", "Maestro") -> {
+            if (currentTransferSum >= 150_000 || userTransfersInMonth >= 600_000)
+                throw IllegalArgumentException("Вы превысили лимит! Ваш перевод = $currentTransferSum, ваши переводы в этом месяце = $userTransfersInMonth")
+            else if (currentTransferSum <= 75_000) {
+                0
+            } else {
+                currentTransferSum * (0.6 / 100) + 20
+            }
+        }
+        cardIssuer in listOf("Visa", "Мир") -> {
+            if (currentTransferSum >= 150_000 || userTransfersInMonth >= 600_000)
+                throw IllegalArgumentException("Вы превысили лимит! Ваш перевод = $currentTransferSum, ваши переводы в этом месяце = $userTransfersInMonth")
+            val percent2Decimal = 0.75 / 100
+            val fee = currentTransferSum * percent2Decimal
+            if (fee <= 35) {
+                35.00
+            } else {
+                fee
+            }
+        }
+        else -> throw IllegalArgumentException("Карта $cardIssuer не поддерживается!")
     }
+    return fee.toDouble()
 }

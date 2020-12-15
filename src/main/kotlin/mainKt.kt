5d26f4bab06e31c1e1ed8f0a2fa272e2334d11fb
unknown <alexander.peshkov27@gmail.com>
2020-12-15 06:50:49 +0000

    Добавить копейки (Double)

--- src/main/kotlin/mainKt.kt
+++ src/main/kotlin/mainKt.kt
@@ -1,32 +1,38 @@
 import java.lang.IllegalArgumentException
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.util.*
+import kotlin.math.round
 
 fun main() {
     val scanner = Scanner(System.`in`).useLocale(Locale.US)
     println("Введите тип карты (VkPlay, MasterCard, Maestro, Visa, Мир):")
     val cardIssuer = scanner.next()
     println("Введите cумму предыдущих переводов в этом месяце:")
-    val lastTransferSum = scanner.nextInt()
+    val lastTransferSum = scanner.nextDouble()
     println("Введите cумму для перевода:")
-    val currentTransferSum = scanner.nextInt()
-    println("Ваша комиссия = ${payFee(cardIssuer, lastTransferSum, currentTransferSum)} копейек")
-
+    val currentTransferSum = scanner.nextDouble()
+    println("Ваша комиссия = ${payFee(cardIssuer, lastTransferSum, currentTransferSum)} копеек")
 }
 
-fun payFee(cardIssuer: String = "VkPlay", lastTransferSum: Int, currentTransferSum: Int):  Double {
+fun payFee(cardIssuer: String = "VkPlay", lastTransferSum: Double, currentTransferSum: Double):  Double {
+    val df = DecimalFormat("#.###")
+    df.decimalFormatSymbols = DecimalFormatSymbols.getInstance(Locale.ENGLISH)
+    df.roundingMode = RoundingMode.CEILING
     println("Расчет для $cardIssuer")
     val userTransfersInMonth = lastTransferSum + currentTransferSum
     val fee = when {
         cardIssuer.equals("VkPlay") -> {
             if (currentTransferSum >= 15_000 && (userTransfersInMonth <= 40_0000))
                 throw IllegalArgumentException("Вы превысили лимит! Ваш перевод = $currentTransferSum, ваши переводы в этом месяце = $userTransfersInMonth")
-            else 0
+            else 0.0
         }
         cardIssuer in listOf("MasterCard", "Maestro") -> {
             if (currentTransferSum >= 150_000 || userTransfersInMonth >= 600_000)
                 throw IllegalArgumentException("Вы превысили лимит! Ваш перевод = $currentTransferSum, ваши переводы в этом месяце = $userTransfersInMonth")
             else if (currentTransferSum <= 75_000) {
-                0
+                0.0
             } else {
                 currentTransferSum * (0.6 / 100) + 20
             }
@@ -44,5 +50,5 @@ fun payFee(cardIssuer: String = "VkPlay", lastTransferSum: Int, currentTransferS
         }
         else -> throw IllegalArgumentException("Карта $cardIssuer не поддерживается!")
     }
-    return fee.toDouble()
+    return df.format(fee).toDouble()
 }
